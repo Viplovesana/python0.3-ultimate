61475994d382b8d5f2addb0bccd5ca6449203e85
Viplovesana <viplovesana90@gmail.com>
2026-03-25 17:38:37 +0530

    input

---
+++ chapter2/04_typeC.py
@@ -0,0 +1,14 @@
+# Typecasting.....................
+
+'''a = "3.5"
+b = float(a)
+t = type(b)
+print(t)
+'''
+
+# Input-function..............
+
+a = int(input("Enter the 1st number :-"))
+b = int(input("Enter the second number :-"))
+
+print("the sum of both the number is :-", a + b)

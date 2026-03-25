ac24381e08fcac541c76d06a820a8a58cc71375e
Viplovesana <viplovesana90@gmail.com>
2026-03-25 22:22:06 +0530

    initial commit

---
+++ chapter2/05_problem5.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the 1st number"))
+b = int(input("Enter the 2nd number"))
+print("the average of these two no is :-",(a+b)/2)
+
+
+s = int(input("Enter the number"))
+print("the square of the number is :-",s*2)

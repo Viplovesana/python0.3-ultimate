220a78d24f7ccf780af9b1ad1eff80e8fcf4e6ee
Viplovesana <viplovesana90@gmail.com>
2026-03-26 12:27:25 +0530

    initial commit

---
+++ chapter3/01_string_intro.py
@@ -0,0 +1,17 @@
+# slicing..............
+
+name = "viplove"
+nameshort = name[0:3]
+print(nameshort)
+char1 = name[1]
+print(char1)
+
+name = "VIPLOVE"
+print(name[0:4])
+
+#["0 1 2 3 4 5 6"]
+#["V I P L O V E"]
+#[-7-6-5-4-3-2-1 ]
+print(name[-4:-1]) #negative indices
+
+print(name[3:6])

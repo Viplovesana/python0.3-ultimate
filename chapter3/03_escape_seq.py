a14142409ddf569b48fcc8a1284518c1ae6b1a87
Viplovesana <viplovesana90@gmail.com>
2026-03-26 16:53:50 +0530

    initial commit

---
+++ chapter3/03_escape_seq.py
@@ -0,0 +1,13 @@
+# ESCAPE SEQUANCE CHARACTERS..................
+
+# \n, \t, \'', \\....................
+
+
+a = "viplove is a bad boy\nbut not a good boy"
+print(a)
+
+print("hello\tworld")
+
+print("this is \"viplove\"")
+
+print("this is a backslashe : \\")
---
+++ chapter3/practice_set.py
@@ -0,0 +1,30 @@
+# practice ....
+
+# ..................
+'''name = input("Enter your name :-")
+
+print(f"good evening,{name}")'''
+# .................
+
+letter = '''Dear <|name|>,
+            You are selected!
+            <|Date|>'''
+
+'''print(letter.replace("<|name|>","Viplove sana").replace("<|Date|>","26 March 2026"))'''
+# ..................
+
+# rind the double space of you string...
+
+name = "hey this is  me viplove Sana"
+print(name.find("  "))
+
+
+# replace the double space from the string with single space
+
+name = "hey this is   me viplove Sana"
+print(name.replace("   "," "))
+
+# wap to formate the following letter using escapwe sequence char..
+
+letters = "Dear Viplove, this python course is nice. Thanks! "
+print("Dear Viplove,\n this python course is nice.\n Thanks! ")

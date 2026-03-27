2c56768267bd3f196e0db9cf04d5689c4ee8ad7a
Viplovesana <viplovesana90@gmail.com>
2026-03-27 23:33:01 +0530

    initial commit

--- chapter4/practice_set.py
+++ chapter4/practice_set.py
@@ -1,8 +1,8 @@
 "practise set of list and tuple"
 
 # "store seven fruits in list"
-'''newlist = []
 
+'''newlist = []
 l1 = input("Enter the fruits name :-")
 newlist.append(l1)
 l2 = input("Enter the fruits name :-")
@@ -15,13 +15,11 @@ l5 = input("Enter the fruits name :-")
 newlist.append(l5)
 l6 = input("Enter the fruits name :-")
 newlist.append(l6)
-
 print(newlist)'''
-
 
 # WAp to accept marks of a 6 student and display them in a sorted manner
 
-stuMarks = []
+'''stuMarks = []
 s1 = int(input("Enter 1st student marks :-"))
 stuMarks.append(s1)
 s2 = int(input("Enter 1st student marks :-"))
@@ -35,5 +33,26 @@ stuMarks.append(s5)
 s6 = int(input("Enter 1st student marks :-"))
 stuMarks.append(s6)
 stuMarks.sort()
-print(stuMarks)
+print(stuMarks)'''
+
+# check that a tuple type cannat be changed in python........
+
+'''tuplee = (12,"viplove",3,"rohan","piyush","divyansh")
+
+findIndex = tuplee.index("rohan")
+print(findIndex)'''
+# print(tuplee.index(12))\
+
+# WAP to sum a list with 4 numbers......
+
+l1  = [34,65,7677,4565]
+
+print(sum(l1))
+
+# WAP to count the number of zeros in the following tuple
+
+tuplee = (0 ,34,34,0,53,6,0,3,0,)
+
+print(tuplee.count(0))
+
 
---
+++ chapter5/01_dict.py
@@ -0,0 +1,16 @@
+profile = {
+    "name":"viplove",
+    "email":"viplovesana90@gmail.com",
+    "department":"email marketing",
+    "salary":20000
+}
+'''print(profile.items())
+print(profile.keys())
+print(profile.values())
+print(profile.update({"department":"python developer"}))
+print(profile)
+print(profile.update({"salary":"60000"}))
+print(profile)'''
+
+print(profile.get("email"))
+print(profile["email"])

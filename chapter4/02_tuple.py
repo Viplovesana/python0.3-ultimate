b06ba90257e93a89e6fbe2ea18922202205ee0f3
Viplovesana <viplovesana90@gmail.com>
2026-03-27 21:13:41 +0530

    initial commit

---
+++ chapter4/02_tuple.py
@@ -0,0 +1,13 @@
+t1 = (1,23,543,5433,23,False,"viplove")
+print(t1)
+print(type(t1))
+
+print(t1.count(23))# it shows 2 count in tuple
+
+print(t1.index(5433))#it shows the index num
+
+print(t1[3:6])
+
+print(len(t1))
+
+print(t1*2)

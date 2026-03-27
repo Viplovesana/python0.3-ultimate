d59b4634625277fc908542cf81ad90fc74600585
Viplovesana <viplovesana90@gmail.com>
2026-03-27 21:39:21 +0530

    initial commit

---
+++ chapter4/practice_set.py
@@ -0,0 +1,39 @@
+"practise set of list and tuple"
+
+# "store seven fruits in list"
+'''newlist = []
+
+l1 = input("Enter the fruits name :-")
+newlist.append(l1)
+l2 = input("Enter the fruits name :-")
+newlist.append(l2)
+l3 = input("Enter the fruits name :-")
+newlist.append(l3)
+l4 = input("Enter the fruits name :-")
+newlist.append(l4)
+l5 = input("Enter the fruits name :-")
+newlist.append(l5)
+l6 = input("Enter the fruits name :-")
+newlist.append(l6)
+
+print(newlist)'''
+
+
+# WAp to accept marks of a 6 student and display them in a sorted manner
+
+stuMarks = []
+s1 = int(input("Enter 1st student marks :-"))
+stuMarks.append(s1)
+s2 = int(input("Enter 1st student marks :-"))
+stuMarks.append(s2)
+s3 = int(input("Enter 1st student marks :-"))
+stuMarks.append(s3)
+s4 = int(input("Enter 1st student marks :-"))
+stuMarks.append(s4)
+s5 = int(input("Enter 1st student marks :-"))
+stuMarks.append(s5)
+s6 = int(input("Enter 1st student marks :-"))
+stuMarks.append(s6)
+stuMarks.sort()
+print(stuMarks)
+

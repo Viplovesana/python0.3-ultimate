7f5c60d29d51865e1cf3dc7a8ffaaddafe9f070b
Viplovesana <viplovesana90@gmail.com>
2026-03-29 16:03:02 +0530

    initial commit

---
+++ chapter5/02_sets.py
@@ -0,0 +1,14 @@
+
+s = {876, 45,1, 5, 32, 54, 5, 5, 5, "viplove"}
+print(s)
+
+s.add(566)
+print(s,type(s))
+
+print(len(s))
+
+s.remove(54) #it removes the 54 from the set
+print(s)
+
+s.pop()
+print(s)

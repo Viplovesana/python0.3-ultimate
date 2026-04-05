35a6b0eec48c9cde55262f08e8e8fba6d023fb8a
Viplovesana <viplovesana90@gmail.com>
2026-04-05 20:48:23 +0530

    initial commit

--- chapter6/01_conditionals.py
+++ chapter6/01_conditionals.py
@@ -1,6 +1,11 @@
 a = int(input("Enter your age :-"))
 
-if(a>18):
+if(a>=18):
     print("you are above the age of concent")
+    print("good for you")
+elif(a<0):
+    print("you are entering an invalid age")   
+elif(a==0):
+    print("you are entering 0 which is not a valid age")     
 else:
     print("you are below the age of concent")    

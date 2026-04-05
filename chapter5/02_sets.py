ed856af1ffac1b4718d21370a5970c46feb219ca
Viplovesana <viplovesana90@gmail.com>
2026-04-05 11:44:33 +0530

    initial commit

--- chapter5/02_sets.py
+++ chapter5/02_sets.py
@@ -1,5 +1,5 @@
 
-s = {876, 45,1, 5, 32, 54, 5, 5, 5, "viplove"}
+'''s = {876, 45,1, 5, 32, 54, 5, 5, 5, "viplove"}
 print(s)
 
 s.add(566)
@@ -14,4 +14,33 @@ s.pop()
 print(s)
 
 s.clear()
-print(s)
+print(s)'''
+
+# ...........SET_UNION_AND_INTERTSECTION.....
+'''
+s1 = {1, 4, 54 ,67}
+s2 = {7, 5, 1, 67}
+
+print(s1.union(s2))
+
+print(s1.intersection(s2)) #intersection makes a new set where it pics commom values from set1 and set2'''
+
+# NEW SETS ......................
+
+'''s1 = {}
+print(s1)'''
+
+s1 = {2,5,7,64,75,86}
+
+s2 = {7,86,54,43,543,42}
+
+'''print(s1.union(s2))
+print(s1.intersection(s2))
+print(len(s1))
+s1.remove(64)
+print(s1)'''
+print(s2.pop())
+
+# create Empty set.......
+EmSet = set()
+print(type(EmSet))

8b204dc3074cc4004388823bbf2cbb7baac69534
Viplovesana <viplovesana90@gmail.com>
2026-04-08 23:30:08 +0530

    initial commit

--- chapter7/01_loops.py
+++ chapter7/01_loops.py
@@ -83,7 +83,7 @@ while(i < 11):
 #         print("the no is prime")
 
 # SAME QUE WITH WHILE LOOP.........
-n = int(input("Enter the number:-"))
+'''n = int(input("Enter the number:-"))
 if n <= 1:
     print("not a prime no")
 else:
@@ -94,7 +94,24 @@ else:
             break
         i=i+1
     else:
-        print("prime no")
+        print("prime no")'''
+
+# WAP to find the sum of first n natural numbers using while loop
+
+'''n=int(input("Enter the num:-"))
+i = 1
+sum = 0
+while(i<=n):
+    sum = sum + i 
+    i=i+1
+print(sum)    
+'''
+'''n=int(input("Enter the num:-"))
+sum = 0
+for i in range(1,n+1):
+    sum = sum + i
+print(sum)'''
+
 
 
 

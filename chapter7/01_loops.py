c725d44405955cd6650e27e2315cb2dacd80079e
Viplovesana <viplovesana90@gmail.com>
2026-04-08 21:36:23 +0530

    initial commit

---
+++ chapter7/01_loops.py
@@ -0,0 +1,18 @@
+
+
+# FOR-LOOP
+'''for i in range(1,20+1):
+    print(i)'''
+#itrate the list using for loop....
+
+'''l = ["viplove","sujal","monu","praful"]
+for i in l:
+    print(i)
+
+t = ("viplove",23,"sujal",22,"praful",34,2)
+for i in t:
+    print(i)'''
+
+# s = "viplove"
+# for i in s:
+#     print(i)
---
+++ chapter7/02_while_loop.py
@@ -0,0 +1,21 @@
+# i = 1
+
+# while(i < 10):
+#     print(i)
+#     i = i + 1
+
+'''while(i<51):
+    print(i)
+    i=i+1
+'''
+
+'''i = 0
+while(i < 6):
+    print("viplove")
+    i=i+1'''
+
+l = [1,"viplove","rohan","shubhi","pranita",89]
+i = 0
+while(i<len(l)):
+    print(l[i])
+    i = i +1    
